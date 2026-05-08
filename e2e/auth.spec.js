946d116e091157a8b830ecb37090533a801bf903
wzbk1 <161694279+wzbk1@users.noreply.github.com>
2026-05-08 10:02:45 +0200

    Update auth.spec.js

--- e2e/auth.spec.js
+++ e2e/auth.spec.js
@@ -3,5 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('homepage loads', async ({ page }) => {
   await page.goto('/');
 
+  await expect(page).toHaveTitle(/EduSYNC Home/);
   await expect(page.locator('body')).toContainText('OK');
 });
